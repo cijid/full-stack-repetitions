b1dfca9db519b3268b0cbd8605b0a15fa8f1a5a3
Cijid <mmkcijid@gmail.com>
2026-09-02 12:44:43 -0600

    Completed Checkpoint 2

--- backend/seeds/favorites.js
+++ backend/seeds/favorites.js
@@ -1,13 +1,18 @@
 /**
  * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
+ * @returns { Promise<void> }
  */
-exports.seed = async function(knex) {
+exports.seed = async function (knex) {
   // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
+  await knex("favorites").del();
+  await knex("favorites").insert([
+    {
+      id: 1,
+      title: "The Matrix",
+      main_character: "Neo",
+      year_released: "1999",
+    },
+    { id: 2, title: "Batman", main_character: "Batman", year_released: "1990" },
+    { id: 3, title: "It", main_character: "Pennywise", year_released: "2022" },
   ]);
 };
